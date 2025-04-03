ddfe386cd787476f87e2b70f1142e21f5cd6f5d6
Christoph Ungricht <christoph.ungricht@outlook.com>
2025-04-03 15:01:58 -0300

    refactor: Use ES6 modules.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- cout/build_web.sh
+++ cout/build_web.sh
@@ -11,7 +11,7 @@ emcc -o build/cout.js \
   -Os -Wall \
   -lm \
   -I/usr/include/SDL2 -D_REENTRANT -lSDL2 -lSDL2_ttf \
-  -sUSE_GLFW=3 -sUSE_SDL=2 -sUSE_SDL_TTF=2 -sASYNCIFY -sMODULARIZE=1 -sEXPORT_NAME=createCout -sEXPORTED_FUNCTIONS=_runGame,_sdlSendLeftMouseButtonPressed \
+  -sUSE_GLFW=3 -sUSE_SDL=2 -sUSE_SDL_TTF=2 -sASYNCIFY -sMODULARIZE=1 -sEXPORT_ES6=1 -sEXPORT_NAME=createCout -sEXPORTED_FUNCTIONS=_runGame,_sdlSendLeftMouseButtonPressed \
   --embed-file ./Lato-Regular.ttf
 
 cp build/cout.js build/cout.wasm .
